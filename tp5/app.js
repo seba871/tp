cfce65aed2a444342099e9da25f0b2179679bfa5
alumno <51085682@ivt.edu.ar>
2026-06-04 09:07:51 -0300

    4 de junio

---
+++ tp5/app.js
@@ -0,0 +1,10 @@
+let n1=40
+let n2=40
+let suma=n1+n2
+alert ("el resultado de la suma es:" + suma)
+let resta=3-1
+alert ("el resultado de la resta es:" + resta)
+let divicion=3/3
+console.log ("el resultado de la divicion es:" + divicion)
+let multiplicacion=4*4
+console.log ("el resultado de la multiplicacion es:" + multiplicacion)
