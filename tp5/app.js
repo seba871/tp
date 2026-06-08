fc3a3a1ab413de5641b9c0b9e677477b3e72807f
seba871 <51085682@ivt.edu.ar>
2026-06-08 11:16:00 -0300

    Cambios JS

--- tp5/app.js
+++ tp5/app.js
@@ -10,3 +10,21 @@ alert ("El resultado de la suma es: " + suma)
 alert ("El resultado de la resta es: " + resta)
 console.log ("El resultado de la multiplicación es: " + multiplicacion)
 console.log ("El resultado de la división es: "+ division)
+let nombreAlumno = "jorge"
+let apellidoAlumno = "george"
+let edadAlumno = 67
+console.log ("El alumno: "+nombreAlumno+" "+apellidoAlumno+" tiene "+edadAlumno+" años.")
+let precio1 = 65
+let precio2 = 87
+let precio3 = 999
+let total = precio1+precio2+precio3
+console.log ("El total a pagar es de: $"+total)
+let comidaFavorita = "queque"
+let colorFavorito = "azul"
+let numeroFavorito = "17"
+console.log ("Mi comida favorita es el "+comidaFavorita+", mi color favorito es el "+colorFavorito+" y mi número favorito es el "+numeroFavorito)
+let pais
+pais = "Argentina"
+console.log (pais)
+pais = "Brasil"
+console.log (pais)
